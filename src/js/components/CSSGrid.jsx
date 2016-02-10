c0432a0fa933e53c2a4b877f25e681395f713c0c
Dan Train <dantrain@gmail.com>
2016-02-10 00:15:49 +0000

    Some success with the CSSGrid enter/exit

--- src/js/components/CSSGrid.jsx
+++ src/js/components/CSSGrid.jsx
@@ -3,40 +3,104 @@ import ReactTransitionGroup from 'react-addons-transition-group';
 import simpleLayout from '../layouts/simple';
 import * as simpleEnterExit from '../enter-exit-styles/simple';
 
+const timeout = 600;
+
 const GridItem = React.createClass({
 
+  getInitialState() {
+    return {
+      isLeaving: false,
+      style: {
+        scale: 1,
+        opacity: 1
+      }
+    };
+  },
+
+  componentWillMount() {
+    this.setEndStyle(this.props);
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setEndStyle(nextProps);
+  },
+
+  componentWillUnmount() {
+    clearTimeout(this.leaveTimeout);
+    clearTimeout(this.enterTimeout);
+  },
+
   componentWillAppear(done) {
+    this.setEndStyle(this.props);
     done();
   },
 
   componentWillEnter(done) {
+    clearTimeout(this.leaveTimeout);
+
+    this.setState({
+      isLeaving: false,
+      style: {
+        ...this.props.position,
+        ...this.props.gridProps.enter(
+          this.props, this.props.gridProps, this.props.gridState)
+      }
+    });
+
+    this.enterTimeout = setTimeout(() => {
+      this.setEndStyle(this.props);
+    }, 0);
+
     done();
   },
 
   componentWillLeave(done) {
-    done();
+    this.leaveTimeout = setTimeout(() => {
+      this.setState({
+        isLeaving: true,
+        style: {
+          ...this.state.style,
+          ...this.props.gridProps.exit(
+            this.props, this.props.gridProps, this.props.gridState)
+        }
+      });
+
+      this.leaveTimeout = setTimeout(done, timeout);
+    }, 0);
+  },
+
+  setEndStyle(props) {
+    if (!this.state.isLeaving) {
+      this.setState({
+        style: {
+          ...props.position,
+          scale: 1,
+          opacity: 1
+        }
+      });
+    }
   },
 
   render() {
-    const item = React.Children.only(this.props.children);
-    const { position: { x, y } } = this.props;
-    const { style } = item.props;
 
-    const scale = 1;
-    const opacity = 1;
+    const item = React.Children.only(this.props.children);
+    const { style: { x, y, scale, opacity } } = this.state;
+    const { style: itemStyle } = item.props;
 
     const transform = `translate(${x}px, ${y}px) scale(${scale})`;
 
     return React.cloneElement(item, {
+      ref: el => this.el = el,
       style: {
-        ...style,
+        ...itemStyle,
         position: 'absolute',
         top: 0,
         left: 0,
         opacity,
         transform,
-        WebkitTransform: transform,
-        MsTransform: transform
+        // WebkitTransform: transform,
+        // MsTransform: transform,
+        transition: `opacity ${timeout}ms ease-out, transform ${timeout}ms ease-out`
       }
     });
   }
@@ -73,12 +137,6 @@ export default React.createClass({
     this.doLayout(nextProps);
   },
 
-  componentDidUpdate() {
-    setTimeout(() => {
-      console.log(document.querySelectorAll('.grid-item').length);
-    }, 100);
-  },
-
   doLayout(props) {
     const { positions, gridWidth, gridHeight } =
       props.layout(React.Children.toArray(props.children)
@@ -88,18 +146,16 @@ export default React.createClass({
   },
 
   render() {
-    const { component, style, children, ...rest } = this.props;
-
+    const { component, style, children, enter, exit, ...rest } = this.props;
     const items = React.Children.toArray(children);
-
     const { positions, gridWidth, gridHeight } = this.state;
-
-    console.log(`positions: ${positions.length}, items: ${items.length}`);
 
     const wrappedItems = items.map((item, i) =>
       <GridItem
         key={item.key}
         position={positions[i]}
+        gridProps={this.props}
+        gridState={this.state}
       >
         {item}
       </GridItem>);
--- src/js/demos/CSSMeasuredResponsive.jsx
+++ src/js/demos/CSSMeasuredResponsive.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import d3Array from 'd3-array';
+import makeResponsive from '../higher-order-components/makeResponsive';
+import measureItems from '../higher-order-components/measureItems';
 import CSSGrid from '../components/CSSGrid';
 import pinterestLayout from '../layouts/pinterest';
+import * as fromCenter from '../enter-exit-styles/fromCenter';
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const ipsum = `Hashtag hoodie food truck XOXO gastropub asymmetrical.
+Viral actually sartorial thundercats fixie next level. Ethical skateboard
+put a bird on it bespoke, brunch small batch photo booth fashion axe
+actually cronut poutine fanny pack microdosing church-key. Post-ironic
+90's pug, master cleanse keytar normcore aesthetic viral crucifix selvage
+gastropub. Echo park yr organic typewriter blog. Health goth literally
+cornhole microdosing fanny pack, bespoke kinfolk heirloom ennui viral
+dreamcatcher. Offal VHS helvetica meh.`;
+
+const ResponsiveGrid = makeResponsive(
+  measureItems(CSSGrid), {
+    maxWidth: 1920,
+    minPadding: 100
+  }
+);
 
 export default React.createClass({
 
   getDefaultProps() {
     return {
-      minItems: 2
+      minItems: 10
     };
   },
 
@@ -39,19 +58,17 @@ export default React.createClass({
   },
 
   render() {
-    const itemWidth = 200;
-
     const items = this.state.data.map(d => {
-      const height = (d.letter.charCodeAt(0) % 3) * 50 + 100;
+      const content = ipsum.slice(0, (d.letter.charCodeAt(0) % 3 + 1) * 50);
 
       return (
         <li
           className="grid-item"
-          style={{ height, width: itemWidth }}
           key={d.letter}
-          itemRect={{ height }}
+          style={{ width: 200 }}
         >
           <h3>{d.letter.toUpperCase()} - {parseInt(d.number, 10)}</h3>
+          <p>{content}</p>
         </li>
       );
     });
@@ -61,17 +78,18 @@ export default React.createClass({
         <button
           onClick={this.handleShuffle}
         >Randomize</button>
-        <CSSGrid
+        <ResponsiveGrid
           className="grid"
           component="ul"
-          columns={3}
-          columnWidth={itemWidth}
+          columnWidth={200}
           gutterWidth={10}
           gutterHeight={10}
           layout={pinterestLayout}
+          enter={fromCenter.enter}
+          exit={fromCenter.exit}
         >
           {items}
-        </CSSGrid>
+        </ResponsiveGrid>
       </div>
     );
   }
