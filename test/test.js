00aafc586e24284791b20569b5e89e7dd2ce5f1a
Dan Train <dantrain@gmail.com>
2016-03-19 20:10:45 +0000

    Test both grid components

--- test/test.js
+++ test/test.js
@@ -3,26 +3,52 @@ import { shallow } from 'enzyme';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import SpringGrid from '../src/components/SpringGrid';
+import CSSGrid from '../src/components/CSSGrid';
 
 chai.use(chaiEnzyme());
 const expect = chai.expect;
 
-describe('<SpringGrid />', function() {
+describe('Grid components common features', function() {
+  const grids = [
+    { name: 'SpringGrid', component: SpringGrid },
+    { name: 'CSSGrid', component: CSSGrid }
+  ];
 
-  it('Renders children', function() {
-    const wrapper = shallow(
-      <div>
-        <SpringGrid
-          columns={4}
-          columnWidth={150}
-        >
-          <span className="item"></span>
-          <span className="item"></span>
-        </SpringGrid>
-      </div>
-    );
+  grids.forEach(function({ name, component: Grid }) {
 
-    expect(wrapper).to.have.exactly(2).descendants('.item');
+    describe(`<${name} />`, function() {
+
+      it('Renders children', function() {
+        const wrapper = shallow(
+          <div>
+            <Grid
+              columns={4}
+              columnWidth={150}
+              duration={2000}
+            >
+              <span className="item"></span>
+              <span className="item"></span>
+            </Grid>
+          </div>
+        );
+
+        expect(wrapper).to.have.exactly(2).descendants('.item');
+      });
+
+      it('Can change tag name', function() {
+        const wrapper = shallow(
+          <Grid
+            component="ul"
+            columns={4}
+            columnWidth={150}
+            duration={2000}
+          />
+        );
+
+        expect(wrapper).to.have.tagName('ul');
+      });
+
+    });
+
   });
-
 });
