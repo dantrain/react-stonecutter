874c0cc9b0c994b504643d9afb9e587e5dc8ef98
Dan Train <dantrain@gmail.com>
2016-06-12 15:39:23 +0100

    Remove images example for now

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/ImagesApp';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
