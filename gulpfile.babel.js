a49bdbe41b1f411173c75aa6d4c7e00273235806
Dan Train <dantrain@gmail.com>
2017-06-25 16:24:55 +0100

    Upgrade to webpack 3

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,16 @@
+import path from 'path';
 import gulp from 'gulp';
 import gutil from 'gulp-util';
 import notifier from 'node-notifier';
 import { create as browserSyncCreate } from 'browser-sync';
 import webpack from 'webpack';
+import nodeExternals from 'webpack-node-externals';
 import eslint from 'gulp-eslint';
 import filter from 'gulp-filter';
 import rename from 'gulp-rename';
 import shell from 'gulp-shell';
 import runSequence from 'run-sequence';
 import assign from 'lodash.assign';
-import packageJson from './package.json';
 
 const browserSync = browserSyncCreate();
 
@@ -22,87 +23,78 @@ browserSync.use({
 
 const sharedWebpackConfig = {
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /dist|public|node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   watch: false
 };
 
 const libWebpackConfig = assign({}, sharedWebpackConfig, {
-  entry: './src/index.js',
+  entry: path.join(__dirname, './src/index.js'),
   output: {
-    path: './lib',
+    path: path.join(__dirname, './lib'),
     filename: 'react-stonecutter.js',
     library: 'reactStonecutter',
     libraryTarget: 'commonjs2'
   },
-  externals: [
-    ...Object.keys(packageJson.dependencies),
-    ...Object.keys(packageJson.peerDependencies)
-  ].reduce((acc, val) => {
-    acc[val] = val;
-    return acc;
-  }, {}),
+  externals: [nodeExternals()],
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
       }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin()
+    })
   ]
 });
 
 const demoWebpackConfig = assign({}, sharedWebpackConfig, {
-  entry: './demo/src/main.jsx',
+  entry: path.join(__dirname, './demo/src/main.jsx'),
   output: {
-    path: './demo/public',
+    path: path.join(__dirname, './demo/public'),
     filename: 'demo.js'
   }
 });
 
-// var devCompiler = webpack([
-//   assign({}, libWebpackConfig, { devtool: 'inline-source-map' }),
-//   assign({}, demoWebpackConfig, { devtool: 'inline-source-map' })
-// ]);
-
 const devCompiler = webpack([libWebpackConfig, demoWebpackConfig]);
 
-gulp.task('webpack', done => {
+gulp.task('webpack', (done) => {
   let firstTime = true;
 
-  devCompiler.watch({
-    aggregateTimeout: 100
-  }, (err, stats) => {
-    if (err) {
-      notifier.notify({ title: 'Webpack Error', message: err });
-      throw new gutil.PluginError('webpack', err);
-    }
+  devCompiler.watch(
+    {
+      aggregateTimeout: 100
+    },
+    (err, stats) => {
+      if (err) {
+        notifier.notify({ title: 'Webpack Error', message: err });
+        throw new gutil.PluginError('webpack', err);
+      }
 
-    const jsonStats = stats.toJson();
+      const jsonStats = stats.toJson();
 
-    if (jsonStats.errors.length > 0) {
-      notifier.notify({ title: 'Webpack Error', message: jsonStats.errors[0] });
-    } else if (jsonStats.warnings.length > 0) {
-      notifier.notify({ title: 'Webpack Warning', message: jsonStats.warnings[0] });
-    }
+      if (jsonStats.errors.length > 0) {
+        notifier.notify({ title: 'Webpack Error', message: jsonStats.errors[0] });
+      } else if (jsonStats.warnings.length > 0) {
+        notifier.notify({ title: 'Webpack Warning', message: jsonStats.warnings[0] });
+      }
 
-    gutil.log(gutil.colors.cyan('[webpack]'), stats.toString({
-      chunks: false,
-      version: false,
-      colors: true
-    }));
+      gutil.log(
+        gutil.colors.cyan('[webpack]'),
+        stats.toString({
+          chunks: false,
+          version: false,
+          colors: true
+        })
+      );
 
-    if (jsonStats.assets[0].name === 'demo.js') {
       browserSync.reload();
 
       if (firstTime) {
@@ -110,7 +102,7 @@ gulp.task('webpack', done => {
         done();
       }
     }
-  });
+  );
 });
 
 gulp.task('browser-sync', ['webpack', 'demo-html-css'], () => {
@@ -124,11 +116,10 @@ gulp.task('browser-sync', ['webpack', 'demo-html-css'], () => {
 });
 
 gulp.task('demo-html-css', () => {
-  const sliderCssFilter = filter('node_modules/rc-slider/assets/index.css',
-                               { restore: true });
+  const sliderCssFilter = filter('node_modules/rc-slider/assets/index.css', { restore: true });
 
-  return gulp.src(['demo/src/*.@(html|css)',
-    'node_modules/rc-slider/assets/index.css'])
+  return gulp
+    .src(['demo/src/*.@(html|css)', 'node_modules/rc-slider/assets/index.css'])
     .pipe(sliderCssFilter)
     .pipe(rename('rc-slider.css'))
     .pipe(sliderCssFilter.restore)
@@ -137,44 +128,40 @@ gulp.task('demo-html-css', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch('src/js/**/*', e => {
-    const path = gutil.colors.magenta(e.path.substring(e.path.lastIndexOf('/') + 1));
+  gulp.watch('src/js/**/*', (e) => {
+    const watchPath = gutil.colors.magenta(e.path.substring(e.path.lastIndexOf('/') + 1));
     const type = gutil.colors.cyan(`${e.type}...`);
-    gutil.log(`${path} ${type}`);
+    gutil.log(`${watchPath} ${type}`);
   });
 
   gulp.watch('demo/src/*.@(html|css)', ['demo-html-css']);
 });
 
-gulp.task('gh-pages-start', shell.task([
-  'git stash',
-  'git checkout gh-pages'
-]));
+gulp.task('gh-pages-start', shell.task(['git stash', 'git checkout gh-pages']));
 
-gulp.task('copy-demo-to-root', () =>
-  gulp.src('./demo/public/**/*')
-    .pipe(gulp.dest('./')));
+gulp.task('copy-demo-to-root', () => gulp.src('./demo/public/**/*').pipe(gulp.dest('./')));
 
-gulp.task('gh-pages-end', shell.task([
-  'git add index.html demo.js *.css',
-  'git commit --amend --no-edit',
-  'git push github gh-pages --force',
-  'git checkout master',
-  'git stash apply'
-]));
+gulp.task(
+  'gh-pages-end',
+  shell.task([
+    'git add index.html demo.js *.css',
+    'git commit --amend --no-edit',
+    'git push github gh-pages --force',
+    'git checkout master',
+    'git stash apply'
+  ])
+);
 
-gulp.task('gh-pages', done => {
+gulp.task('gh-pages', (done) => {
   runSequence('gh-pages-start', 'copy-demo-to-root', 'gh-pages-end', done);
 });
 
 gulp.task('lint', () =>
-  gulp.src([
-    'src/**/*.js*(x)',
-    'demo/src/**/*.js*(x)',
-    'test/**/*.js'
-  ])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError()));
+  gulp
+    .src(['src/**/*.js*(x)', 'demo/src/**/*.js*(x)', 'test/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
 
 gulp.task('default', ['browser-sync', 'watch']);
